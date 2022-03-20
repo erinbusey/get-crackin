47aef2b72380bbcca38799bfe8a1bb70de10e80e
Erin Busey <75703589+erinbusey@users.noreply.github.com>
2022-03-20 19:01:46 +0100

    Made forEach for for forecast Loop/ added notes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,30 @@
 const apiKey = "a22a698f45541df9c65139ef895958ef\n";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-
 getCurrentPosition();
 
+// Get current position / Lat/Long
 function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(showPosition)
+    // Get Coords from Navigator Geolocation and then execute the "showPosition" Function
+    navigator.geolocation.getCurrentPosition(getCityName);
 }
-function showPosition(position) {
+
+// Get the City Name from the openweathermap API
+function getCityName(position) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+    //displayForecast(position.coords);
     axios.get(apiUrl).then(displayCity);
 }
+
+// Display the City from the API Request Response in id="cityName" (HTML)
 function displayCity(response) {
-    let cityName = document.querySelector("#cityName");
-    cityName.innerHTML = response.data.name;
+    let cityNameElement = document.querySelector("#cityName"); // This line gets the HTML Element into Javascript
+    cityNameElement.innerHTML = response.data.name; // Here we exchange the content of the City Element with the Name of the City that we get from the API
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${response.data.name}&appid=${apiKey}&units=metric`; // Request the weather from the City Name (response.data.name)
+    axios.get(apiUrl).then(displayTemperatureAndTimeAndIcon); // Make the API Call, execute function displayTemperature with the response
 }
 
-
-let formElement = document.querySelector("#locationForm");
-formElement.addEventListener("submit", showCityWeather);
-function showCityWeather (event) {
-    event.preventDefault();
-    let city = document.querySelector("#cityInput").value;
-    console.log(city);
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-}
-
-
-function displayTemperature(response) {
-    console.log(response);
+// Display Weather Info / Time / Big Icon
+function displayTemperatureAndTimeAndIcon(response) {
+    // Set the weather data in HTML
     let temperatureElement = document.querySelector("#bigTemp");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#cityName");
@@ -43,13 +38,26 @@ function displayTemperature(response) {
 
     // Set Date & Time
     let dateElement = document.querySelector("#dateTime");
-    dateElement.innerHTML = formatDate(response.data.dt*1000);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
     // Set Icon
     let iconElement = document.querySelector("#iconMain");
-    iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src",`https://vigorous-mcclintock-84a9de.netlify.app/icons/${response.data.weather[0].icon}.svg`);
+}
+// End of initial functions
+
+// Form Submit
+let formElement = document.querySelector("#locationForm");
+formElement.addEventListener("submit", showCityWeather);
+function showCityWeather(event) {
+    event.preventDefault();
+    let city = document.querySelector("#cityInput").value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 }
 
+
+// Format Date from Unix Timestamp
 function formatDate(timeStamp) {
     let date = new Date(timeStamp);
     console.log(date);
@@ -63,6 +71,30 @@ function formatDate(timeStamp) {
     let minutes = date.getMinutes();
 
     return `${day} ${hours}:${minutes}`;
+}
+
+// Display 7 day Forecast
+function displayForecast(coords) {
+    let forecastElement = document.querySelector("#displayForecast");
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let forecastHTML = "";
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + `   
+                <div class="col-2">
+                    <div class="weather-forecast-item">
+                        <div class="item-day">
+                            ${day}
+                        </div>
+                        <div class="item-icon">
+                            <i class="fas fa-bolt"></i>
+                        </div>
+                        <div class="item-temp">
+                            20°
+                        </div>
+                    </div>
+                </div>`;
+    });
+    forecastElement.innerHTML = forecastHTML;
 }
 
 
